50a2e75e6a0565063489c730e244ddb25f5b77c4
Hong Shen Zhou <hong.shenzhou@gmail.com>
2015-06-23 13:47:38 +0800

    Puts everything inside a document.ready wrapper

--- scripts/main.js
+++ scripts/main.js
@@ -40,29 +40,36 @@ var sha256_hasher = function(data, type) {
         return sjcl.codec.base64.fromBits(sha256_raw);
     }
 }
+$( document ).ready(function() {
+    //Reads value from input box in html page and passes it on to relevent functions
+    $("#hash_button").click(function() {
+        var token = token_preparser($("#token_input").val());
+        var password = $("#password_input").val();
+        var hash = sha256_hasher((token + password), "base64");
+        $("#hash_output").val(hash);
 
-//Reads value from input box in html page and passes it on to relevent functions
-$("#hash_button").click(function() {
-    var token = token_preparser($("#token_input").val());
-    var password = $("#password_input").val();
-    var hash = sha256_hasher((token + password), "base64");
-    $("#hash_output").val(hash);
+    });
 
+    $("#options_button").click(function() {
+        $("#options_menu").slideToggle(500);
+    });
+
+    $("#enforce_lowercase").click(function(){
+       if($(this).is(":checked")){
+           localStorage.setItem("enforce_lowercase", true);
+       }
+       else if($(this).is(":not(:checked)")){
+           localStorage.setItem("enforce_lowercase". false);
+
+       }
+    });
+
+    $("#strip_whitespace").click(function(){
+        if($(this).is(":checked")){
+            localStorage.setItem("strip_whitespace", true);
+        }
+        else if($(this).is(":not(:checked)")){
+            localStorage.setItem("strip_whitespace", false);
+        }
+    });
 });
-
-$("#options_button").click(function() {
-    $("#options_menu").slideToggle(500);
-});
-
-
-if ($("#strip_whitespace").attr('checked')) {
-    console.log("strip_whitespace is enabled");
-    localStorage.setItem("strip_whitespace", true);
-    console.log(localStorage.getItem("strip_whitespace"));
-}
-var lowercase = $("#enforce_lowercase").attr('checked')
-if ($("#enforce_lowercase").attr('checked')) {
-    console.log("enforce_lowercase is enabled");
-    localStorage.setItem("enforce_lowercase", true);
-    console.log(localStorage.getItem("enforce_lowercase"));
-}
