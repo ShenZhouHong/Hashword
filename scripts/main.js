70bb0c50a67d0ba606589d15d4ddb8ee3cf28dbd
Hong Shen Zhou <hong.shenzhou@gmail.com>
2015-06-23 13:44:11 +0800

    Added basic settings for preparer processing

--- scripts/main.js
+++ scripts/main.js
@@ -42,10 +42,27 @@ var sha256_hasher = function(data, type) {
 }
 
 //Reads value from input box in html page and passes it on to relevent functions
-    $("#button").click(function() {
-        var token = token_preparser($("#token_input").val());
-        var password = $("#password_input").val();
-        var hash = sha256_hasher((token + password), "base64");
-        $("#hash_output").val(hash);
+$("#hash_button").click(function() {
+    var token = token_preparser($("#token_input").val());
+    var password = $("#password_input").val();
+    var hash = sha256_hasher((token + password), "base64");
+    $("#hash_output").val(hash);
 
-    });
+});
+
+$("#options_button").click(function() {
+    $("#options_menu").slideToggle(500);
+});
+
+
+if ($("#strip_whitespace").attr('checked')) {
+    console.log("strip_whitespace is enabled");
+    localStorage.setItem("strip_whitespace", true);
+    console.log(localStorage.getItem("strip_whitespace"));
+}
+var lowercase = $("#enforce_lowercase").attr('checked')
+if ($("#enforce_lowercase").attr('checked')) {
+    console.log("enforce_lowercase is enabled");
+    localStorage.setItem("enforce_lowercase", true);
+    console.log(localStorage.getItem("enforce_lowercase"));
+}
