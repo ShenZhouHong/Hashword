958d750a910c499ad465d42b4f37623c4519ed08
Hong Shen Zhou <hong.shenzhou@gmail.com>
2015-06-25 12:39:54 +0800

    Minor javascript cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
+// Preparses token for the sake of consistency
 var token_preparser = function(token, trim, lowercase) {
-    // Preparses token for the sake of consistency
     if (trim == true) {
         var token = token.trim();
     }
@@ -9,8 +9,8 @@ var token_preparser = function(token, trim, lowercase) {
     return token
 }
 
+// Creates different encodings of the sha256 hash (base64 or hex)
 var sha256_hasher = function(data, type) {
-    // Creates different encodings of the sha256 hash (base64 or hex)
     if (type == "hex") {
         return sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(data));
     }
@@ -18,17 +18,53 @@ var sha256_hasher = function(data, type) {
         return sjcl.codec.base64.fromBits(sjcl.hash.sha256.hash(data));
     }
 }
+
+// Main javascript code body - executes on document load ready
 $(document).ready(function() {
+
+    // Automatically focuses on first inputbox to start with
     $("#token_input").focus();
+
+    // Uses slick.js to start slide panel sliding effect
+    $(".wrapper").slick({
+
+        // Sets sliding speed
+        speed: 200,
+
+        // Binds forward and backward buttons
+        nextArrow: $(".forward"),
+        prevArrow: $(".backward"),
+
+        // Sliding and slide view settings
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
+        // Disables various hipster drag settings
+        draggable: false,
+        swipe: false,
+        swipeToSlide: false,
+        touchMove: false,
+
+        // Misc. slide.js settings for slider window
+        variableWidth: true,
+        infinite: false,
+        centerMode: false,
+        initialSlide: 1
+    });
+
     //Reads value from input box in html page and passes it on to relevent functions
     $(".inputbox").on('input', function() {
+
         // Checks in case inputs are empty
         if ($("#token_input").val() === "" || $("#password_input").val() === "") {
+
             // Do nothing or clears output
             $("#hash_output").val("");
         }
+
         // If inputs are not empty...
         else {
+
             // Creates hash and writes to inputbox
             $("#hash_output").val(sha256_hasher(token_preparser($("#token_input").val(), localStorage.getItem("strip_whitespace"), localStorage.getItem("enforce_lowercase")) + $("#password_input").val(), "base64"));
 
@@ -48,24 +84,6 @@ $(document).ready(function() {
                 });
             });
         }
-    });
-
-    //Slick Slider
-    $(".wrapper").slick({
-        nextArrow: $(".forward"),
-        prevArrow: $(".backward"),
-        variableWidth: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: false,
-        centerMode: false,
-        initialSlide: 1,
-        speed: 200,
-        draggable: false,
-        swipe: false,
-        swipeToSlide: false,
-        touchMove: false,
-
     });
 
     // Toggles preparser enforce_lowercase option
